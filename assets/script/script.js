1f0cb56a694104adde655c4463f8446d619d57fd
Thangga7 <beencules@gmail.com>
2024-02-09 16:53:52 +0700

    update

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,25 +3,20 @@ var container = document.querySelector(".container");
 var btnOk = document.querySelector(".agree");
 var btnKhong = document.querySelector(".disagree");
 
-function swapButtons() {
-  var okPos = { top: btnOk.offsetTop, left: btnOk.offsetLeft };
-  var khongPos = { top: btnKhong.offsetTop, left: btnKhong.offsetLeft };
-
-  btnKhong.style.position = "absolute";
-  btnOk.style.position = "absolute";
-  btnKhong.style.transition = "0.5s";
-  btnOk.style.transition = "0.5s";
-  btnKhong.style.top = okPos.top + "px";
-  btnKhong.style.left = okPos.left + "px";
-  btnOk.style.top = khongPos.top + "px";
-  btnOk.style.left = khongPos.left + "px";
-}
-swapButtons();
-
-btnKhong.addEventListener("mouseover", swapButtons);
-btnKhong.addEventListener("click", swapButtons);
-
 btnOk.addEventListener("click", function () {
   qrCode.style.display = "block";
   container.style.display = "none";
+});
+
+var btnKhong = document.querySelector(".disagree");
+
+btnKhong.addEventListener("click", function () {
+  var result = confirm("Bạn phải nhấn nút OK!");
+  if (result) {
+    // Người dùng đã nhấn nút OK
+    // Thực hiện hành động tương ứng tại đây
+  } else {
+    // Người dùng đã nhấn nút Cancel hoặc đã đóng hộp thoại
+    // Thực hiện hành động tương ứng tại đây
+  }
 });
