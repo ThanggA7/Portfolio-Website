6de5e5c25d5c75c957c546194acd8ece2591c060
Thangga7 <beencules@gmail.com>
2024-02-19 11:46:26 +0700

    responsive

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,3 +7,20 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     });
   });
 });
+let nameElement = document.querySelector(".name");
+
+nameElement.addEventListener("mouseover", function () {
+  this.style.opacity = "0";
+  setTimeout(() => {
+    this.textContent = "Noazuynh";
+    this.style.opacity = "1";
+  }, 500);
+});
+
+nameElement.addEventListener("mouseout", function () {
+  this.style.opacity = "0";
+  setTimeout(() => {
+    this.textContent = "Nhu Thang";
+    this.style.opacity = "1";
+  }, 500);
+});
